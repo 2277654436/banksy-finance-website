d2a94b8622be0a8909721313c393d2e8048c8c59
linchengyang <18960807585@163.com>
2021-04-29 23:46:25 +0800

    Add Banksy logo

--- src/App.js
+++ src/App.js
@@ -3,12 +3,15 @@ import {Row, Col} from 'antd';
 import 'antd/dist/antd.css';
 import Top from "./pages/top";
 import Foot from "./pages/foot";
+import logo from './image/logo.png'
 
 function App() {
   return (
     <div className="App">
       <div className="menu">
         <Row>
+          <Col span={2}/>
+          <Col span={1}><img src={logo} style={{width: '35px'}}/></Col>
           <Col span={12}/>
           <Col span={1}><a href={"#"}>HOME</a></Col>
           <Col span={1}><a href={"#"}>WHITEPAPER</a></Col>
