29d8fb599a4922c72ddfe843eb3d7e86a60e7092
maybis <615763004@qq.com>
2021-07-13 18:01:55 +0800

    modify style of top page

--- src/pages/top/index.js
+++ src/pages/top/index.js
@@ -1,40 +1,63 @@
 import './index.css'
 import service from '../../image/service/service.png'
-import topBac from '../../image/top/topBac.png'
+import MainBG from '../../image/top/main_bg.png'
 import virtualland from '../../image/top/virtualland.png'
 import solible from '../../image/top/solible.png'
 import cryptokitties from '../../image/top/cryptokitties.png'
 import raible from '../../image/top/rarible.png'
 import opensea from '../../image/top/opensea.png'
+import Icon1 from '../../image/top/icon1.png'
+import Icon2 from '../../image/top/icon2.png'
+import Icon3 from '../../image/top/icon3.png'
+import Icon4 from '../../image/top/icon4.png'
+import Icon5 from '../../image/top/icon5.png'
+import Icon6 from '../../image/top/icon6.png'
+
 
 
 function Top() {
   return(
     <div className="top">
-      <div className="top-title" id="scc1">
+      {/*<div className="top-title" id="scc1">
         The First Decentralized NFT Financial Hub
-      </div>
+      </div>*/}
       <div className="top-content">
         <div className="top-content-child">
-          Decentralized
+          NFT Mortgage
         </div>
         <div className="top-content-child">
-          NFT Financial Center.
+          Lending Hub
         </div>
       </div>
       <div className="topBac">
-        <img src={topBac} />
+        <img src={ MainBG } />
       </div>
       <div className="top-foot">
-        <div>Unleashing the full potential of NFT</div>
-        <div>through finance sercives</div>
+        <div>The first AI-driven NFT pool-based</div>
+        <div>lending platform to lead a revolution</div>
+        <div>of the NFT market</div>
       </div>
-      <div className="topBacStart">
+      {/*<div className="topBacStart">
         <img src={virtualland}/>
         <img src={solible}/>
         <img src={cryptokitties}/>
         <img src={raible}/>
         <img src={opensea}/>
+      </div>*/}
+      <div className="top-icon-bg">
+          <img src={Icon1} alt=""/>
+          <img src={Icon2} alt=""/>
+          <img src={Icon3} alt=""/>
+          <img src={Icon4} alt=""/>
+          <img src={Icon5} alt=""/>
+
+
+
+      </div>
+      <div className="explore-button">
+          <div className="button-border">
+              Explore
+          </div>
       </div>
       <div className="service" id="scc2">
         <img className="serviceImg" src={service} data-aos="fade-right" />
