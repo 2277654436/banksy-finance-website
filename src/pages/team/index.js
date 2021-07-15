38b64859a093d7afcc9846724f6994c7969db124
maybis <615763004@qq.com>
2021-07-15 14:10:26 +0800

    modify and add team member pictures

--- src/pages/team/index.js
+++ src/pages/team/index.js
@@ -6,6 +6,8 @@ import cto from '../../image/team/2.png'
 import cmo from '../../image/team/3.png'
 import engineer1 from '../../image/team/4.png'
 import engineer2 from '../../image/team/5.png'
+import architect from '../../image/team/6.png'
+import algorithm from '../../image/team/7.png'
 import linkedin from '../../image/team/linkedin.png'
 import github from '../../image/team/github-link.png'
 
@@ -68,15 +70,31 @@ function Team() {
 
             </div>
 
-            <div className="temContent" data-aos="fade-up" style={{marginTop:'35rem'}}>
+
+
+            <div className="temContent2" data-aos="fade-up" style={{marginTop:'35rem'}}>
                 <div className="teamContent-item">
                     <div className="teamContent-img">
-
+                        <img src={architect} />
                     </div>
-                    <div className="position"></div>
-                    <div className="teamItem-name"></div>
+                    <div className="position">Blockchain Architect</div>
+                    <div className="teamItem-name">Wei Jiang</div>
                     <div className="linkdin">
-                        <a href={"https://www.linkedin.com/in/clink-li-aa1ba418a/"} target='_blank'>
+                        <a href={"https://github.com/GleipnirJ"} target='_blank'>
+                            <img src={github} alt=""/>
+                        </a>
+                    </div>
+                </div>
+
+                <div className="teamContent-item">
+                    <div className="teamContent-img">
+                        <img src={algorithm} />
+                    </div>
+                    <div className="position">Chief Algorithm Expert</div>
+                    <div className="teamItem-name">Roger Luo</div>
+                    <div className="linkdin">
+                        <a href={"https://github.com/luojie1024"} target='_blank'>
+                            <img src={github} alt=""/>
                         </a>
                     </div>
                 </div>
@@ -86,7 +104,7 @@ function Team() {
                         <img src={engineer1} />
                     </div>
                     <div className="position">Front end Engineer</div>
-                    <div className="teamItem-name">PeiHuang Guo</div>
+                    <div className="teamItem-name"uo>PeiHuang Guo</div>
                     <div className="linkdin">
                         <a href={"https://github.com/Disperito"} target='_blank'>
                             <img src={github} alt=""/>
@@ -101,7 +119,7 @@ function Team() {
                     <div className="teamItem-name">ChengYang Lin</div>
                     <div className="linkdin">
                         <a href={"https://github.com/linchengyang1116"} target='_blank'>
-                            <img src={github} />
+                            <img src={github} alt=""/>
                         </a>
                     </div>
                 </div>
